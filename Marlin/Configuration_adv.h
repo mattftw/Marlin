6ab40df7334811709a0a2e158581a17ed11b3472
mattftw <mattftw@users.noreply.github.com>
2022-01-23 00:35:53 +1030

    enable useful features

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1522,10 +1522,10 @@
 
   // Allow international symbols in long filenames. To display correctly, the
   // LCD's font must contain the characters. Check your selected LCD language.
-  //#define UTF_FILENAME_SUPPORT
+  #define UTF_FILENAME_SUPPORT
 
   // This allows hosts to request long names for files and folders with M33
-  //#define LONG_FILENAME_HOST_SUPPORT
+  #define LONG_FILENAME_HOST_SUPPORT
 
   // Enable this option to scroll long filenames in the SD card menu
   //#define SCROLL_LONG_FILENAMES
