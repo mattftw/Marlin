2e59b21b33cc05d0f870cb3793a262dc74479d4e
mattftw <mattftw@users.noreply.github.com>
2022-05-26 19:06:36 +0930

    disable print pointer
    enable other useful features

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1374,10 +1374,10 @@
   //#define LCD_DECIMAL_SMALL_XY
 
   // Add an 'M73' G-code to set the current percentage
-  //#define LCD_SET_PROGRESS_MANUALLY
+  #define LCD_SET_PROGRESS_MANUALLY
 
   // Show the E position (filament used) during printing
-  //#define LCD_SHOW_E_TOTAL
+  #define LCD_SHOW_E_TOTAL
 #endif
 
 // LCD Print Progress options
